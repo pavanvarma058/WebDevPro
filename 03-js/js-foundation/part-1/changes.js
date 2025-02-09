643aa61640fb9b276a96d64829e70fa219d9625e
Pavan Varma <pavanvarma058@gmail.com>
2025-02-09 23:00:37 +0530

    learned the const keyword in js

---
+++ 03-js/js-foundation/part-1/changes.js
@@ -0,0 +1,11 @@
+let gameName = "spiderman";
+
+gameName = "batman";
+
+console.log(gameName);
+
+const userName = "PavanVarma"; // once we defined we cannot changed it back
+
+userName = "Yeswanth";
+
+console.log(userName);
