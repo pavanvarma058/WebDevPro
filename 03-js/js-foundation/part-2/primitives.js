2d68395cd8eebf0bbff26172812533f005e70847
Pavan Varma <pavanvarma058@gmail.com>
2025-02-09 23:56:31 +0530

    learned about primitives

---
+++ 03-js/js-foundation/part-2/primitives.js
@@ -0,0 +1,36 @@
+// Number
+
+let balance = 20;
+let anotherBalance = new Number(25);
+console.log(balance);
+
+console.log(typeof balance);
+console.log(typeof anotherBalance);
+
+// boolean
+let isActive = true;
+let isReallyActive = new Boolean(true);
+
+// null and undefined
+
+// let firstName; null
+let lastName;
+console.log(lastName); // undefined
+
+// String
+
+let myString = "Hello";
+let myString1 = "hola";
+let userName = "pavan";
+
+let oldGreet = myString + " Pavan";
+console.log(oldGreet);
+
+let greetMessage = `Hello ${userName}!`;
+console.log(greetMessage);
+let demoOne = `Value is ${2 * 2}`;
+console.log(demoOne);
+
+const sm1 = Symbol("Pavan");
+const sm2 = Symbol("Varma");
+console.log(sm1);
