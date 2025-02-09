ed5ce8f0f3b7d80f48a5e36879726f269f7139cc
Pavan Varma <pavanvarma058@gmail.com>
2025-02-09 23:23:55 +0530

    learned about logical, assignment, operations, and operator precdence in JavaScript

---
+++ 03-js/js-foundation/part-1/logical.js
@@ -0,0 +1,13 @@
+// && -> AND
+// || -> OR
+// ! -> reverse
+
+let isLoggedIn = true;
+let isPaid = false;
+
+console.log(isLoggedIn && isPaid);
+
+let isEmailUser = true;
+let isGoogleUser = false;
+
+console.log(isEmailUser || isGoogleUser);
---
+++ 03-js/js-foundation/part-1/operations.js
@@ -0,0 +1,28 @@
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus;
+
+let addition = 4 + 5;
+let subtraction = 9 - 3;
+let mul = 3 * 5;
+let div = 8 / 2;
+let remainder = 9 % 2;
+let expo = 2 ** 4; // 2 ^ 4
+
+let myScore = 110;
+myScore++;
+
+let credits = 25;
+credits--;
+
+// comparison operators
+
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2);
+console.log(num1 != num3);
+console.log(num1 > num3);
+console.log(num1 < num3);
