a011f7b8a7ea9d28ca61a77e5f92a70801c7a88d
Pavan Varma <pavanvarma058@gmail.com>
2025-02-09 22:56:09 +0530

    learned about output types and data types in js

---
+++ 03-js/js-foundation/part-1/datatype.js
@@ -0,0 +1,33 @@
+// comments
+console.log("hello");
+/*
+
+String
+Number
+Boolean
+BigInt
+
+Undefined - I don't have a definition
+Null - empty
+
+Object
+Symbol
+*/
+
+// var score = 102; older way
+
+let score = 102; // new way
+let name = "Pavan Varma";
+let isLoggedIn = false;
+
+// Object
+
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"];
+
+let user = {
+  firstName: "Pavan",
+  lastName: "Varma",
+};
+
+let getScore = score;
+console.log(getScore);
---
+++ 03-js/js-foundation/part-1/printing.js
@@ -0,0 +1,8 @@
+// types of output methods that are used to print
+
+console.log("Hello");
+console.log("Chai");
+process.stdout.write("hello");
+process.stdout.write("chai");
+
+console.table({ city: "Vijayawada" });
