c251781896dbc5c8725cc3cd28bfaed8567d9c13
Pavan Varma <pavanvarma058@gmail.com>
2025-02-12 17:12:12 +0530

    learned about prototypes and constructor functions in JS

---
+++ 03-js/js-foundation/part-6/constructorFunction.js
@@ -0,0 +1,46 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
+
+let myCar = new Car("Toyota", "Camry");
+// console.log(myCar);
+
+let myNewCar = new Car("Tata", "Safari");
+// console.log(myNewCar);
+
+function Tea(type) {
+  this.type = type;
+  this.describe = function () {
+    return `this is a cup of ${this.type}`;
+  };
+}
+
+let lemonTea = new Tea("Lemon Tea");
+console.log(lemonTea.describe());
+
+function Animal(species) {
+  this.species = species;
+}
+
+Animal.prototype.sound = function () {
+  return `${this.species} makes sound`;
+};
+
+let dog = new Animal("Dog");
+console.log(dog.sound());
+
+function Drink(name) {
+  if (!new.target) {
+    throw new Error("Drink must be called with 'new' keyword");
+  }
+  this.name = name;
+}
+
+let tea = new Drink("Tea");
+let coffee = Drink("Coffee");
