9d8505413a98f78cb22c06d0d5ca6df0a39130f9
Pavan Varma <pavanvarma058@gmail.com>
2025-02-27 12:46:24 +0530

    completed weather project

--- 03-js/js-projects/02_weather/script.js
+++ 03-js/js-projects/02_weather/script.js
@@ -9,20 +9,51 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const API_KEY = "c5f964edbdf610401a53320fdb6bfd5e";
 
-  getWeatherBtn.addEventListener("click", function () {
+  getWeatherBtn.addEventListener("click", async () => {
     const city = cityInput.value.trim();
     if (!city) return;
+
+    // it may throw an error
+    // server/databse is always in another contient
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
   });
 
-  function fetchWeatherData(city) {
+  async function fetchWeatherData(city) {
     // gets the data
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+    const respone = await fetch(url);
+    console.log(typeof respone);
+    console.log("Response", respone);
+
+    if (!respone.ok) {
+      throw new Error("City not found");
+    }
+    const data = await respone.json();
+    return data;
   }
 
-  function displayWeatherData(weatherData) {
+  function displayWeatherData(data) {
     // displays the data
+    console.log(data);
+    const { name, main, weather } = data;
+    cityNameDisplay.textContent = name;
+    temparatureDisplay.textContent = `Temparature: ${main.temp}`;
+    descriptionDisplay.textContent = `Weather: ${weather[0].description}`;
+    // unlock the display
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
   }
 
   function showError() {
     // shows the error message
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
   }
 });
